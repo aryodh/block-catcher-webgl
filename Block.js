7585f3901a5942f030a92d5c0225174f701523f5
Aryo Tinulardhi <ardh.yodh@gmail.com>
2020-03-17 14:01:33 +0700

    adding highscore and change shape

--- Block.js
+++ Block.js
@@ -10,9 +10,9 @@ var BlockVShaderID = "vertexShader";
 var BlockData = {
 	red: {
 		color: "red",
-		speed: 1,
-		size: 2.5,
-		points: 1
+		speed: 2,
+		size: 1.5,
+		points: -5
 	},
 	blue: {
 		color: "blue",
@@ -26,9 +26,9 @@ var BlockData = {
 		size: 1,
 		points: 25
 	},
-	key: function(n) {
-        return this[Object.keys(this)[n-1]];
-    }
+	key: function (n) {
+		return this[Object.keys(this)[n - 1]];
+	}
 };
 
 var minSize = 0.1;
@@ -37,70 +37,98 @@ var Block = function (gl) {
 	this.gl = gl;
 	this.blockID = Math.floor((Math.random() * 3) + 1);
 	this.blockData = BlockData.key(this.blockID);
-	this.startx = (Math.random() * (1-(minSize*this.blockData.size)+1))-1;
-	this.points = [vec2(this.startx,1.01+(minSize*this.blockData.size)),vec2(this.startx+(minSize*this.blockData.size),1.01+(minSize*this.blockData.size)),vec2(this.startx+(minSize*this.blockData.size),1.01),vec2(this.startx,1.01)];
+	this.startx = (Math.random() * (1 - (minSize * this.blockData.size) + 1)) - 1;
+	if (this.blockData.color == "red") {
+		this.points = [
+			vec2(this.startx + (minSize * this.blockData.size), 1.01 + (minSize * this.blockData.size) * 3 / 4),
+			vec2(this.startx + (minSize * this.blockData.size), 1.01 + (minSize * this.blockData.size) / 3),
+			vec2(this.startx + (minSize * this.blockData.size) / 2, 1.01),
+			vec2(this.startx, 1.01 + (minSize * this.blockData.size) / 3),
+			vec2(this.startx, 1.01 + (minSize * this.blockData.size) * 3 / 4),
+
+		];
+	} else if (this.color == "blue") {
+		this.points = [
+			vec2(this.startx, 1.01 + (minSize * this.blockData.size)),
+			vec2(this.startx + (minSize * this.blockData.size), 1.01 + (minSize * this.blockData.size)),
+			vec2(this.startx + (minSize * this.blockData.size), 1.01),
+			vec2(this.startx, 1.01),
+
+		];
+	} else {
+		this.points = [
+			vec2(this.startx + (minSize * this.blockData.size) * 2 / 3, 1.01 + (minSize * this.blockData.size) * 3 / 4),
+			vec2(this.startx + (minSize * this.blockData.size), 1.01 + (minSize * this.blockData.size) / 2),
+			vec2(this.startx + (minSize * this.blockData.size) / 2, 1.01),
+			vec2(this.startx, 1.01 + (minSize * this.blockData.size) / 2),
+			vec2(this.startx + (minSize * this.blockData.size) / 3, 1.01 + (minSize * this.blockData.size) * 3 / 4),
+
+		];
+	}
+
+
 	this.leftTopMax = 0;
 	this.rightBottomMax = 2;
 	this.shiftX = 0;
 	this.shiftY = 0;
-	this.deltaTrans = 0.010*this.blockData.speed;
+	this.deltaTrans = 0.010 * this.blockData.speed;
 	this.buffer = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, this.buffer );
-	gl.bufferData( gl.ARRAY_BUFFER,	flatten(this.points), gl.STATIC_DRAW );
-	
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(this.points), gl.STATIC_DRAW);
+
 	this.attachShaders();
 }
 
-Block.prototype.attachShaders = function() {
+Block.prototype.attachShaders = function () {
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, document.getElementById(BlockVShaderID).text);
-    gl.compileShader(vertexShader);
-	
+	gl.shaderSource(vertexShader, document.getElementById(BlockVShaderID).text);
+	gl.compileShader(vertexShader);
+
 	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fragShader, document.getElementById(this.blockData.color).text );
+	gl.shaderSource(fragShader, document.getElementById(this.blockData.color).text);
 	gl.compileShader(fragShader);
-	
+
 	this.shaderProgram = gl.createProgram();
 	this.gl.attachShader(this.shaderProgram, vertexShader);
-    this.gl.attachShader(this.shaderProgram, fragShader);
+	this.gl.attachShader(this.shaderProgram, fragShader);
 	this.gl.linkProgram(this.shaderProgram);
 }
 
-Block.prototype.attachVariables = function() {
+Block.prototype.attachVariables = function () {
 	var myPosition = this.gl.getAttribLocation(this.shaderProgram, "myPosition");
-	this.gl.vertexAttribPointer( myPosition, 2, this.gl.FLOAT, false, 0, 0 );
-	this.gl.enableVertexAttribArray( myPosition );
-	
-	this.xshiftLoc = this.gl.getUniformLocation(this.shaderProgram,"xshift");
-	this.yshiftLoc = this.gl.getUniformLocation(this.shaderProgram,"yshift");
+	this.gl.vertexAttribPointer(myPosition, 2, this.gl.FLOAT, false, 0, 0);
+	this.gl.enableVertexAttribArray(myPosition);
+
+	this.xshiftLoc = this.gl.getUniformLocation(this.shaderProgram, "xshift");
+	this.yshiftLoc = this.gl.getUniformLocation(this.shaderProgram, "yshift");
 }
 
-Block.prototype.moveX = function(forward) {
+Block.prototype.moveX = function (forward) {
 	var change = forward ? this.deltaTrans : -this.deltaTrans;
 	if (this.points[this.leftTopMax][0] + change >= -1.01 && this.points[this.rightBottomMax][0] + change < 1.01) {
 		this.shiftX += change;
-		for (var i=0; i<this.points.length;i++) {
-		this.points[i][0] = this.points[i][0]+change;
+		for (var i = 0; i < this.points.length; i++) {
+			this.points[i][0] = this.points[i][0] + change;
 		}
 	}
 }
 
-Block.prototype.moveY = function(forward, timediff) {
+Block.prototype.moveY = function (forward, timediff) {
 	var change = forward ? this.deltaTrans : -this.deltaTrans;
-	change *= timediff/25;
+	change *= timediff / 25;
 	//console.log(change);
 	this.shiftY += change;
-	for (var i=0; i<this.points.length;i++) {
-		this.points[i][1] = this.points[i][1]+change;
+	for (var i = 0; i < this.points.length; i++) {
+		this.points[i][1] = this.points[i][1] + change;
 	}
 }
 
-Block.prototype.render = function(timediff) {
+Block.prototype.render = function (timediff) {
 	gl.useProgram(this.shaderProgram);
-	gl.bindBuffer( gl.ARRAY_BUFFER, this.buffer );
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 	this.attachVariables();
 	this.moveY(false, timediff);
-	this.gl.uniform1f(this.yshiftLoc,this.shiftY);
+	this.gl.uniform1f(this.yshiftLoc, this.shiftY);
 	this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, this.points.length);
 	//window.requestAnimFrame(this.render);
 }
--- game.js
+++ game.js
@@ -8,9 +8,10 @@ var player;
 var blocks = [];
 var gl;
 
-var display,scoreDisplay,statusDisplay,playDisplay;
+var display, scoreDisplay, statusDisplay, playDisplay;
 
-var score = 0, scoreThreshold = 500;
+var score = 0,
+	scoreThreshold = 500;
 var SetTime = 60;
 var gameStarted = false;
 var lastFrame = performance.now();
@@ -21,15 +22,20 @@ function initializeGame() {
 	scoreDisplay = document.getElementById("score");
 	statusDisplay = document.getElementById("status");
 	playDisplay = document.getElementById("play");
-	
-    var canvas=document.getElementById("gl-canvas");
-    gl=WebGLUtils.setupWebGL(canvas);
-    if (!gl) { alert( "WebGL is not available" ); }
-    
-    gl.viewport(0, 0, 512, 512); // set size of viewport
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); // background black
-    gl.clear(gl.COLOR_BUFFER_BIT); // allows color
-	
+	high_score = document.getElementById("highScore");
+	high_score.textContent = highScore;
+
+
+	var canvas = document.getElementById("gl-canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) {
+		alert("WebGL is not available");
+	}
+
+	gl.viewport(0, 0, 512, 512); // set size of viewport
+	gl.clearColor(0.0, 0.0, 0.0, 1.0); // background black
+	gl.clear(gl.COLOR_BUFFER_BIT); // allows color
+
 	var timestep = lastFrame;
 	player = new Player(gl); // create new player
 	player.render(false, false, 0);
@@ -37,52 +43,51 @@ function initializeGame() {
 	render(timestep);
 }
 
-var keyEnum = { UP:0, DOWN:1, LEFT:2, RIGHT:3 };
+var keyEnum = {
+	UP: 0,
+	DOWN: 1,
+	LEFT: 2,
+	RIGHT: 3
+};
 var keyArray = new Array(4);
 
+// Function for check action when pressing a key
 function keyDown(event) {
 	var keyCode = event.keyCode;
-	
+
 	if (keyCode == 32) { // SPACE
 		event.preventDefault();
-	}
-	else if (keyCode == 37) { // LEFT
+	} else if (keyCode == 37) { // LEFT
 		event.preventDefault();
 		keyArray[keyEnum.LEFT] = true;
-	}
-	else if (keyCode == 38) { // UP
+	} else if (keyCode == 38) { // UP
 		event.preventDefault();
 		keyArray[keyEnum.UP] = true;
-	}
-	else if (keyCode == 39) { // RIGHT
+	} else if (keyCode == 39) { // RIGHT
 		event.preventDefault();
 		keyArray[keyEnum.RIGHT] = true;
-	}
-	else if (keyCode == 40) { // DOWN
+	} else if (keyCode == 40) { // DOWN
 		event.preventDefault();
 		keyArray[keyEnum.DOWN] = true;
 	}
 }
 
+// Function for check action when unpressing a key
 function keyUp(event) {
 	var keyCode = event.keyCode;
 	if (keyCode == 32) { // SPACE
 		event.preventDefault();
 		startGame();
-	}
-	else if (keyCode == 37) { // LEFT
+	} else if (keyCode == 37) { // LEFT
 		event.preventDefault();
 		keyArray[keyEnum.LEFT] = false;
-	}
-	else if (keyCode == 38) { // UP
+	} else if (keyCode == 38) { // UP
 		event.preventDefault();
 		keyArray[keyEnum.UP] = false;
-	}
-	else if (keyCode == 39) { // RIGHT
+	} else if (keyCode == 39) { // RIGHT
 		event.preventDefault();
 		keyArray[keyEnum.RIGHT] = false;
-	}
-	else if (keyCode == 40) { // DOWN
+	} else if (keyCode == 40) { // DOWN
 		event.preventDefault();
 		keyArray[keyEnum.DOWN] = false;
 	}
@@ -90,45 +95,67 @@ function keyUp(event) {
 
 function render(frameStart) {
 	var timestep = frameStart - lastFrame;
-    lastFrame = frameStart;
+	lastFrame = frameStart;
 	gl.clear(gl.COLOR_BUFFER_BIT); // allows color
 	player.render(keyArray[keyEnum.LEFT], keyArray[keyEnum.RIGHT], timestep);
-	for (var i=0;i<blocks.length;i++) {
+	for (var i = 0; i < blocks.length; i++) {
 		blocks[i].render(timestep);
+
+		// Check if the block was catch
 		if (blocks[i].points[blocks[i].rightBottomMax][1] <= player.points[player.leftTopMax][1] && blocks[i].points[blocks[i].leftTopMax][1] >= player.points[player.rightBottomMax][1] &&
 			blocks[i].points[blocks[i].rightBottomMax][0] >= player.points[player.leftTopMax][0] && blocks[i].points[blocks[i].leftTopMax][0] <= player.points[player.rightBottomMax][0]) {
+
+			// Change border color
+			// if get red block (minus point) it will change to red
+			// if get blue or white block (plus point) it will change to green
+			if (blocks[i].blockData.color == 'red') {
+				document.getElementsByTagName("canvas")[0].setAttribute("style", "border: 4px solid red;");
+			} else {
+				document.getElementsByTagName("canvas")[0].setAttribute("style", "border: 4px solid #3EDC81;");
+			}
+
+			// Adding score
 			score += blocks[i].blockData.points;
 			scoreDisplay.textContent = score;
-			blocks.splice(i,1);
+			blocks.splice(i, 1);
 			i--;
-		}
-		else if (blocks[i].points[blocks[i].leftTopMax][1] <= -1.0) {
-			blocks.splice(i,1);
+		} else if (blocks[i].points[blocks[i].leftTopMax][1] <= -1.0) {
+			blocks.splice(i, 1);
 			i--;
 		}
 	}
 	window.requestAnimFrame(render);
 }
 var gameTimer;
+var localStorageName = "crackalien";
+var highScore;
+highScore = localStorage.getItem(localStorageName) == null ? 0 :
+	localStorage.getItem(localStorageName);
+
+
+// Timer function
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    gameTimer = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+	var timer = duration,
+		minutes, seconds;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+	gameTimer = setInterval(function () {
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
 
-        display.textContent = minutes + ":" + seconds;
-		
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		display.textContent = minutes + ":" + seconds;
+
 		SpawnBlocks(timer);
 
-        if (--timer < 0) {
-            timer = 0;
-        }
-    }, 1000);
+		if (--timer < 0) {
+			timer = 0;
+		}
+	}, 1000);
 }
 
+// Start new game
 function startGame() {
 	if (gameStarted == false) {
 		gameStarted = true;
@@ -141,20 +168,32 @@ function startGame() {
 	}
 }
 
+// Spawn new block
 function SpawnBlocks(timer) {
 	if (timer != 0) {
 		blocks.push(new Block(gl));
-	}
-	else {
-		blocks.splice(0,blocks.length);
+	} else {
+		blocks.splice(0, blocks.length);
 		clearInterval(gameTimer);
 		display.textContent = "01:00";
 		gameStarted = false;
 		if (score >= scoreThreshold) {
-			statusDisplay.textContent = "YOU WON!";
-			statusDisplay.style.color = "green";
-		}
-		else {
+			if (score > highScore) {
+				console.log("New Highscore");
+				highScore = score;
+				console.log(highScore);
+				highScore = Math.max(score, highScore);
+				localStorage.setItem(localStorageName, highScore);
+				high_score.textContent = highScore;
+
+				statusDisplay.textContent = "YOU WON! - New High Score : " + highScore;
+				statusDisplay.style.color = "green";
+			} else {
+				statusDisplay.textContent = "YOU WON!";
+				statusDisplay.style.color = "green";
+			}
+
+		} else {
 			statusDisplay.textContent = "YOU LOST!";
 			statusDisplay.style.color = "red";
 		}
